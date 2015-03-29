4ada1d38a4ca7f0aafa6196a66fb7d336f1e62bb
Naoki Morita <namorit@gmail.com>
2015-03-30 03:35:59 +0900

    Update 2015-03-29T18:35:55.918Z

--- angular/directives/clock/clockDirective.js
+++ angular/directives/clock/clockDirective.js
@@ -4,24 +4,27 @@
   clockDirective = function() {
     return {
       templateUrl: "angular/directives/clock/clockDirective.html",
+      scope: {
+        time: "@",
+        text: "@"
+      },
       controller: [
-        '$scope', function($scope) {
-          this.currentTime = function() {
+        '$scope', '$interval', function($scope, $interval) {
+          $scope.time = "--:--:--";
+          this.updateClock = function() {
             var date, hours, minutes, seconds;
             date = new Date();
             hours = date.getHours();
             minutes = date.getMinutes();
             seconds = date.getSeconds();
-            return hours + " : " + minutes + " . " + seconds;
+            return $scope.time = hours + " : " + minutes + " . " + seconds;
           };
-          return this.text = "controller";
+          $scope.text = "controller";
+          return $interval(this.updateClock, 1000);
         }
       ],
-      controllerAs: 'ctrl',
-      link: function(scope, element, attrs, ctrl) {
-        scope.text = "test";
-        return scope.time = ctrl.currentTime();
-      }
+      controllerAs: "clockCtrl",
+      link: function(scope, element, attrs, ctrl) {}
     };
   };
 
